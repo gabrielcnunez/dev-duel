94f0c19e51ad3e6f3c14bb573e7bd8505cd54585
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-09-15 21:14:36 -0700

    Complete duel page with winner

--- client/src/app/duel/duel.component.ts
+++ client/src/app/duel/duel.component.ts
@@ -4,18 +4,18 @@ import { UserService } from 'src/user.service';
 @Component({
   selector: 'app-duel',
   templateUrl: './duel.component.html',
-  styleUrls: ['./duel.component.css']
+  styleUrls: ['./duel.component.css'],
 })
 export class DuelComponent implements OnInit {
-  usernameOne: string = "";
-  usernameTwo: string = "";
+  usernameOne: string = '';
+  usernameTwo: string = '';
   duelUsers: any;
   error: any;
+  winnerIndex: number | null = null;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   receiveUsernameOne(valueEmitted: string) {
     this.usernameOne = valueEmitted;
@@ -25,12 +25,29 @@ export class DuelComponent implements OnInit {
     this.usernameTwo = valueEmitted;
   }
 
+  duelWinner() {
+    if (this.duelUsers && this.duelUsers.length === 2) {
+      const [user1, user2] = this.duelUsers;
+      
+      const score1 = user1.followers + user1['total-stars'] 
+      const score2 = user2.followers + user2['total-stars']
+      
+      console.log(score1);
+      console.log(score2);
+      this.winnerIndex = score1 > score2 ? 0 : 1;
+    }
+  }
+
   async onSubmit() {
     this.error = null;
     this.duelUsers = null;
 
     try {
-      this.duelUsers = await this.userService.duelUsers(this.usernameOne, this.usernameTwo);
+      this.duelUsers = await this.userService.duelUsers(
+        this.usernameOne,
+        this.usernameTwo
+      );
+      this.duelWinner();
     } catch (error) {
       this.error;
       throw error;
